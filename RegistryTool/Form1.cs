a4798f4e1bc9118246e5a382e4657583966fbcf9
Andrei Tudor <tudor.andrei.707@gmail.com>
2024-04-14 14:15:10 +0300

    Added support for windows 11 context menu removal

--- RegistryTool/Form1.cs
+++ RegistryTool/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 using Microsoft.Win32;
@@ -12,29 +13,35 @@ namespace RegistryTool
 		public ToolForm()
 		{
 			InitializeComponent();
-			Start_1();
+            Load += FormLoaded;
 
         }
-        void Start_1()
-		{
-			try
-			{
-				RegistryKey key = Registry.ClassesRoot.OpenSubKey("RegistryTool", true);
-				key.OpenSubKey("ToolKeys", true);
-				key.Close();
-			}
-			catch
-			{
-				RegistryKey key = Registry.ClassesRoot.CreateSubKey("RegistryTool");
-				key = Registry.ClassesRoot.OpenSubKey("RegistryTool", true);
-				key.CreateSubKey("ToolKeys");
-				key.Close();
-				MessageBox.Show("The tool has been installed successfully !\nThis message will not show up again (unless you click the Delete button)!\n" +
-					"(if it does, please contact me on discord : Wizzy#9181)", "RegistryTool",
-					MessageBoxButtons.OK,MessageBoxIcon.Information);
 
+        private void FormLoaded(object sender, EventArgs e)
+        {
+
+			if(!IsWindows11() || PatchApplied())
+			{
+				button4.Enabled = false;
 			}
-		}
+
+            try
+            {
+                RegistryKey key = Registry.ClassesRoot.OpenSubKey("RegistryTool", true);
+                key.OpenSubKey("ToolKeys", true);
+                key.Close();
+            }
+            catch
+            {
+                RegistryKey key = Registry.ClassesRoot.CreateSubKey("RegistryTool");
+                key = Registry.ClassesRoot.OpenSubKey("RegistryTool", true);
+                key.CreateSubKey("ToolKeys");
+                key.Close();
+                MessageBox.Show("The tool has been installed successfully !\nThis message will not show up again (unless you click the Delete button)!", "RegistryTool",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
 
 		private void button1_Click(object sender, EventArgs e)
 		{
@@ -68,10 +75,69 @@ namespace RegistryTool
 				key = Registry.ClassesRoot.OpenSubKey("RegistryTool", true);
 				key.CreateSubKey("ToolKeys");
 				key.Close();
-				MessageBox.Show("The tool has been installed successfully !\nThis message will not show up again !\n(if it does, please contact me on discord : Wizzy#9181)", "RegistryTool",
+				MessageBox.Show("The tool has been installed successfully !\nThis message will not show up again !", "RegistryTool",
 					MessageBoxButtons.OK, MessageBoxIcon.Information);
 
 			}
 		}
-	}
+
+        private bool PatchApplied()
+        {
+            var newKey = Registry.ClassesRoot.OpenSubKey("RegistryTool\\ToolKeys", true);
+            if (newKey is null) return false;
+            if(newKey.GetValue("Win11_Patch") != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsWindows11()
+        {
+            var reg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+
+            var currentBuildStr = (string)reg.GetValue("CurrentBuild");
+            var currentBuild = int.Parse(currentBuildStr);
+
+            return currentBuild >= 22000;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+			var key = Registry.CurrentUser.OpenSubKey(@"SOftware\Classes\CLSID", true);
+			key.CreateSubKey("{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}");
+            key = key.OpenSubKey("{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}", true);
+			key.CreateSubKey("InprocServer32");
+            key.Close();
+
+            var newKey = Registry.ClassesRoot.OpenSubKey("RegistryTool\\ToolKeys", true);
+            newKey.SetValue("Win11_Patch", true);
+            newKey.Close();
+
+            RestartExplorer();
+
+            button4.Enabled = false;
+
+        }
+
+        private void RestartExplorer()
+        {
+            using (Process p = new Process())
+            {
+                p.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "taskkill.exe",
+                    Arguments = "-f -im explorer.exe",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    UseShellExecute = true,
+                    CreateNoWindow = true,
+                };
+                p.Start();
+                p.WaitForExit();
+                p.StartInfo = new ProcessStartInfo("explorer.exe");
+                p.Start();
+            }
+        }
+    }
 }
